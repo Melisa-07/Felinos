df2801997790bcb260f875b229f3e22a485877b5
Melisa Alfonso <alfonsomelisa@hotmail.com>
2024-07-22 17:08:00 -0300

    Test railway 1.0

--- api/obtener_felinos.php
+++ api/obtener_felinos.php
@@ -4,10 +4,10 @@ header('Content-Type: application/json');
 ?>
 
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "GatFinder";
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
