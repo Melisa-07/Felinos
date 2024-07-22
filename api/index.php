3aab0b4719fd8939a9332b596e893df68b863e79
Melisa Alfonso <alfonsomelisa@hotmail.com>
2024-07-22 18:40:07 -0300

    Test Vercel

--- api/index.php
+++ api/index.php
@@ -1,10 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "GatFinder";
+$servername = "mysql.railway.internal";  
+$username = "root";  
+$password = "gZxcaZVgfKMBZalpzaauSnKQXnMAAAqR";  
+$dbname = "railway";  
+$port = "3306"; 
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
   die("Conexión fallida: " . $conn->connect_error);
--- api/obtener_felinos.php
+++ api/obtener_felinos.php
@@ -4,12 +4,13 @@ header('Content-Type: application/json');
 ?>
 
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "GatFinder";
+$servername = "mysql.railway.internal";  
+$username = "root";  
+$password = "gZxcaZVgfKMBZalpzaauSnKQXnMAAAqR";  
+$dbname = "railway";  
+$port = "3306"; 
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
